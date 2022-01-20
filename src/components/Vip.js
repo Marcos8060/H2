08e7d58bce8d284a52e0f92a1cc38a794492f23a
Marcos8060 <marcosgav80@gmail.com>
2022-01-20 17:18:15 +0300

    added a vip section

--- src/components/Vip.js
+++ src/components/Vip.js
@@ -3,7 +3,19 @@ import React from 'react';
 function Vip() {
   return (
       <div className='app_vip'>
-          
+          <div className="container">
+            <div class="row">
+                <div className="col-6">
+                    <div className="background">
+                        <img src="" alt="" />
+                    </div>
+                </div>
+                <div className="col-6">
+                    <h2>Get the VIP experience</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis veniam assumenda aliquam dolores commodi enim tempora. Quaerat ullam repellat dolore.</p>
+                </div>
+            </div>
+          </div>
       </div>
       );
 }
